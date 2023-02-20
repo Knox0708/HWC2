cbd5362ea247c9846d1e3a6bbbe0fa85dcdab54d
Knox0708 <michael.knox@nationwide.co.uk>
2023-02-20 12:09:27 +0000

    Added Secuirty Group

--- main.tf
+++ main.tf
@@ -21,6 +21,9 @@ resource "aws_instance" "example" {
   key_name = "tf-key-pair"
   user_data = file("LogstashInstall.sh")
 
+    vpc_security_group_ids = [aws_security_group.Default.id]
+
+
   
 
   tags = {
@@ -28,6 +31,20 @@ resource "aws_instance" "example" {
   }
 }
 
+#Security Groups
+resource "aws_security_group" "Default" {
+  name_prefix = "Default"
+  description = "Default security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "ssh"
+    cidr_blocks = ["86.22.208.224/32"] #My IP?
+  }
+}
+
+#Keys are below
 resource "aws_key_pair" "tf-key-pair" {
 key_name = "tf-key-pair"
 public_key = tls_private_key.rsa.public_key_openssh
@@ -40,6 +57,14 @@ resource "local_file" "tf-key-pair" {
 content  = tls_private_key.rsa.private_key_pem
 filename = "tf-key-pair"
 }
+
+
+
+
+
+
+
+
 
 # Configure Logstash input
 resource "logstash_input_file" "input" {
