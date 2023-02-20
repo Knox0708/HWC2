25b89ca5e24396cced3509b13af61f33b40e3c19
Knox0708 <michael.knox@nationwide.co.uk>
2023-02-20 16:05:09 +0000

    revert to no logstash

--- main.tf
+++ main.tf
@@ -4,11 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
-    logstash = {
-      source  = "hashicorp/logstash"
-      version = "3.0.0"
-      url     = "https://registry.terraform.io/providers/hashicorp/logstash"
-}
+    # logstash = {
+    #   source  = "hashicorp/logstash"
+    #   version = "3.0.0"
+    #  }
   }
 
   required_version = ">= 1.2.0"
@@ -18,11 +17,13 @@ terraform {
 
 provider aws {
   region     = "us-east-1"
+    access_key = AKIAS5SKF73IX2I4LXFR # Might need .aws_access_key
+    secret_key = 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 # Might need .aws_secret_key
 }
 
-provider "logstash" {
-  endpoint = "http://localhost:9600"
-}
+# provider "logstash" {
+#   source = "hashicorp/logstash"
+# }
 
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
@@ -69,24 +70,24 @@ filename = "tf-key-pair"
 
 
 
-# Configure Logstash input
-resource "logstash_input_file" "input" {
-  path = "*.log" #Need to add log.files
-}
+# # Configure Logstash input
+# resource "logstash_input_file" "input" {
+#   path = "*.log" #Need to add log.files
+# }
 
-# Configure Logstash output
-resource "logstash_output_file" "output" {
-  path = "stashOutput.log"
-}
+# # Configure Logstash output
+# resource "logstash_output_file" "output" {
+#   path = "stashOutput.log"
+# }
 
-# Configure Logstash pipeline
-resource "logstash_pipeline" "Attempt" {
-  config {
-    input {
-      plugin = logstash_input_file.input.id
-    }
-    output {
-      plugin = logstash_output_file.output.id
-    }
-  }
-}
+# # Configure Logstash pipeline
+# resource "logstash_pipeline" "Attempt" {
+#   config {
+#     input {
+#       plugin = logstash_input_file.input.id
+#     }
+#     output {
+#       plugin = logstash_output_file.output.id
+#     }
+#   }
+# }
