e5e1f2b5350f9f7e922c92f1f7210580e963e366
Knox0708 <michael.knox@nationwide.co.uk>
2023-03-01 13:54:40 +0000

    Added Index 1

--- StartupScript.sh
+++ StartupScript.sh
@@ -76,6 +76,19 @@ systemctl daemon-reload
 systemctl enable kibana.service
 systemctl start kibana.service
 
+# Create a new index pattern in Kibana
+curl -X POST "http://localhost:5601/api/saved_objects/index-pattern" \
+  -H 'kbn-xsrf: true' \
+  -H 'Content-Type: application/json' \
+  -d'
+{
+  "attributes": {
+    "title": "log",
+    "timeFieldName": "@timestamp"
+  }
+}'
+
+
 # Install Logstash
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 echo "[logstash-7.x]
