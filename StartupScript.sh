2948dc96efa6ea16e2c995edf77b384d1603dd31
Knox0708 <michael.knox@nationwide.co.uk>
2023-03-01 14:37:48 +0000

    New Pattern

--- StartupScript.sh
+++ StartupScript.sh
@@ -81,12 +81,10 @@ curl -X PUT "http://localhost:9200/logs" -H 'Content-Type: application/json'
 
 # Create a new index pattern in Kibana
 curl -X POST "http://localhost:5601/api/saved_objects/index-pattern" \
-  -H 'kbn-xsrf: true' \
-  -H 'Content-Type: application/json' \
   -d'
 {
   "attributes": {
-    "title": "log",
+    "title": "logs-*",
     "timeFieldName": "@timestamp"
   }
 }'
